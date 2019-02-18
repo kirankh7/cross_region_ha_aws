bdd005135026529bd4ef5f7a0a07c28a5caef736
kiran.haridas <kiran.haridas@ey.com>
2019-02-17 19:22:14 -0800

    Full Wroking region1

--- terraform_ha_stack/modules/instances/tr-group-attachment.tf
+++ terraform_ha_stack/modules/instances/tr-group-attachment.tf
@@ -1,5 +1,12 @@
 resource "aws_lb_target_group_attachment" "attache_tier3" {
+  count = "${var.INSTANCE_COUNT}"
   target_group_arn = "${var.TR_GROUP_NAME[0]}"
-  target_id        = "${element(aws_instance.instance_tier2.*.id, count.index)}"
-  port             = 80
+//  target_id        = "${element(split(",", aws_instance.instance_tier2.*.id), count.index)}"
+  target_id = "${element(split(",", join(",", aws_instance.instance_tier3.*.id)), count.index)}"
+
+//  port             = 80
 }
+//resource "aws_alb_target_group_attachment" "" {
+//  target_group_arn = ""
+//  target_id = ""
+//}
